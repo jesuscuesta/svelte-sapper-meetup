38074175a6adc2d9ff9485cc211f8ed1dc4e82b4
Jesus Cuesta <jesus@jesuscuesta.net>
2020-05-28 00:28:29 +0200

    initial

---
+++ src/routes/marvel.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Marvel</title>
+</svelte:head>
+
+<h1>Somos el equipo de Marvel</h1>
+
+<p><img alt='Preparados' src='https://zezeewithbooks.files.wordpress.com/2019/06/avengers-gif-thor-5.gif'></p>
